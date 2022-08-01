d6dab749c56ca87525076907333f20a6783443fc
aMahanna <anthony.mahanna@gmail.com>
2022-08-01 12:41:10 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,7 +53,8 @@ def pytest_configure(config: Any) -> None:
     db = ArangoClient(hosts=con["url"], http_client=NoTimeoutHTTPClient()).db(
         con["dbName"], con["username"], con["password"], verify=True
     )
-    db.create_database("py310")
+    if db.has_database("py310") is False:
+        db.create_database("py310")
 
     global adbpyg_adapter
     adbpyg_adapter = ADBPyG_Adapter(db, logging_lvl=logging.DEBUG)
